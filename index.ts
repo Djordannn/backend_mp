376552b9a0fd55f551030f5b03fc2ff9e3ca68fe
Djordannn <161820966+Djordannn@users.noreply.github.com>
2024-12-19 12:13:11 +0700

    Add files via upload

--- index.ts
+++ index.ts
@@ -15,7 +15,7 @@ app.get("/", (req: Request, res: Response): any => {
 
 app.get("/branch", async (req: Request, res: Response): Promise<any> => {
   try {
-    const branches = await prisma.branch.findMany();
+    const branches = await prisma.branch.findMany({ where: req.query });
 
     res.status(200).send({
       message: "Get all branch",
@@ -24,7 +24,43 @@ app.get("/branch", async (req: Request, res: Response): Promise<any> => {
     });
   } catch (error) {
     res.status(500).send({
-      message: "Error get data",
+      message: "Error get branch",
+      success: false,
+    });
+  }
+});
+
+app.post("/branch", async (req: Request, res: Response): Promise<any> => {
+  try {
+    const branches = await prisma.branch.create({ data: req.body });
+    res.status(200).send({
+      message: "Add branch success",
+      success: true,
+      result: branches,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: "Error add branch",
+      success: false,
+    });
+  }
+});
+
+app.patch("/branch/:id", async (req: Request, res: Response): Promise<any> => {
+  try {
+    const branches = await prisma.branch.update({
+      data: req.body,
+      where: { id: parseInt(req.params.id) },
+    });
+
+    res.status(200).send({
+      message: "Update branch success",
+      success: true,
+      result: branches,
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: "Error update branch",
       success: false,
     });
   }
