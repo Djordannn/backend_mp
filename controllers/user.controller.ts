bd284eb77ddee3e715fe5e58477ced15c0c7da40
Djordann <djordanfnby@gmail.com>
2024-12-23 11:28:31 +0700

    revamp userController

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from "express";
 import { prisma } from "../config/prisma";
-
+import { sign } from "jsonwebtoken";
 import { hashPassword } from "../utils/hashPassword";
 import { compare, compareSync } from "bcrypt";
+import ResponseHandler from "../utils/response";
 
 export const register = async (req: Request, res: Response): Promise<any> => {
   try {
@@ -10,17 +11,11 @@ export const register = async (req: Request, res: Response): Promise<any> => {
       data: { ...req.body, password: await hashPassword(req.body.password) },
     });
 
-    return res.status(201).send({
-      message: "Registration is success",
-      success: true,
-    });
+    ResponseHandler.success(res, "Registration is success", 201);
   } catch (error: any) {
     console.log(error);
-    return res.status(500).send({
-      message: "Registration is failed",
-      success: false,
-      error,
-    });
+
+    ResponseHandler.error(res, "Your registration is failed", error, 500);
   }
 };
 
@@ -39,14 +34,84 @@ export const login = async (req: Request, res: Response): Promise<any> => {
     if (!comparePass) {
       throw { rc: 401, message: "Wrong password!!" };
     }
+
+    // Generate token
+    const token = sign(
+      { id: findUser.id, email: findUser.email },
+      process.env.TOKEN_KEY || "test"
+    );
+
     return res.status(200).send({
       username: findUser.username,
       email: findUser.email,
+      token,
     });
   } catch (error) {
     console.log(error);
     return res.status(500).send({
       message: "Login is failed",
+      success: false,
+      error,
+    });
+  }
+};
+
+export const update = async (req: Request, res: Response): Promise<any> => {
+  try {
+    const updateUser = await prisma.users.update({
+      where: { id: res.locals.decript.id },
+      data: { ...req.body, password: await hashPassword(req.body.password) },
+    });
+
+    if (!updateUser) {
+      throw { rc: 404, message: "Account is not exist" };
+    }
+    // Generate token
+    const token = sign(
+      { id: updateUser.id, email: updateUser.email },
+      process.env.TOKEN_KEY || "test"
+    );
+
+    return res.status(202).send({
+      message: "Update user success",
+      success: true,
+      result: updateUser,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      message: "Update is failed",
+      success: false,
+      error,
+    });
+  }
+};
+
+export const keepLogin = async (req: Request, res: Response): Promise<any> => {
+  try {
+    // data from middleware token
+    console.log("at keeplogin controller", res.locals.decript);
+    const findUser = await prisma.users.findUnique({
+      where: { id: res.locals.decript.id },
+    });
+
+    if (!findUser) {
+      throw { rc: 404, message: "Account is not exist" };
+    }
+    // Generate token
+    const token = sign(
+      { id: findUser.id, email: findUser.email },
+      process.env.TOKEN_KEY || "test"
+    );
+    return res.status(200).send({
+      username: findUser.username,
+      email: findUser.email,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      message: "Your keeplogin is failed",
       success: false,
       error,
     });
--- routers/user.router.ts
+++ routers/user.router.ts
@@ -1,9 +1,17 @@
 import { Router } from "express";
-import { register, login } from "../controllers/user.controller";
+import {
+  register,
+  login,
+  update,
+  keepLogin,
+} from "../controllers/user.controller";
+import { verifyToken } from "../middleware/verifyToken";
 
 const route = Router();
 
 route.post("/register", register);
 route.post("/login", login);
+route.patch("/update/:id", update);
+route.get("/keep-login", verifyToken, keepLogin);
 
 export default route;
